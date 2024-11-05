0085cfaf7a6d1495136034ad82ffe0515cb602a8
Oliver Thorndike <oliverthorndike@Ryans-MacBook-Air.local>
2024-11-04 23:44:05 -0700

    remove chunkers folder

--- app/chunkers/[username]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Chunker({params}: {params: {username: string}}) {
-    return <div>Chunker Page for {params.username}</div>
-}
--- app/chunkers/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Chunkers() {
-    return <div>Chunkers Category Page</div>
-}
