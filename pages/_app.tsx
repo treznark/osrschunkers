b60bbeac69967901ac268e22c279de3dfe35be00
Oliver Thorndike <oliverthorndike@Ryans-MacBook-Air.local>
2024-11-13 11:57:45 -0700

    adds GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import "@/app/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <GoogleAnalytics measurementId="G-BR55E0B62X" />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
